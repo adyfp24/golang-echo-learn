643df48fe8b068a4023f26e466fb039884f4ee7f
adyfp24 <adyfp24@gmail.com>
2024-01-25 02:06:09 +0700

    feat(main, folder structure): testing simple endpoint and completing folder structure

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
 import (
-	
+	"fmt"
+	"net/http"
+	"github.com/labstack/echo/v4"
 )
+func main(){
+	e := echo.New()
+	e.GET("/",func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "Halo selamat datang Ady, selamat belajar",
+		})
+	})
+	e.Start(":8000")
+	fmt.Println("server berjalan di port 8000")
+}

0eab3107557fd1aac1b2db0895cff5f20e427717
adyfp24 <adyfp24@gmail.com>
2024-01-30 06:47:28 +0700

    feat(author): completing crud author and organizing folder structure

--- database/database.go
+++ database/database.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func InitDB() (*gorm.DB, error) {
 
-	dsn := "root:@tcp(127.0.0.1:3306)/db_go_learn?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/db_go_fiber_learn?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
--- handlers/authorHandler.go
+++ handlers/authorHandler.go
@@ -1,11 +1,9 @@
 package handlers
 
 import (
-	"errors"
 	"github.com/adyfp24/golang-fiber-learn/database"
 	"github.com/adyfp24/golang-fiber-learn/models"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 )
 
 func CreateAuthor(c *fiber.Ctx) error {
@@ -60,14 +58,14 @@ func GetAuthorById(c *fiber.Ctx) error {
 	var author models.Author
 	err := db.Find(&author, id).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return c.Status(404).JSON(fiber.Map{
-				"message": "id tidak ditemukan",
-			})
-		}
 		return c.Status(500).JSON(fiber.Map{
 			"message": "bad gateway",
 			"error":   err.Error(),
+		})
+	}
+	if author.ID == 0 {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "id tidak ditemukan",
 		})
 	}
 	return c.Status(200).JSON(fiber.Map{
@@ -76,25 +74,59 @@ func GetAuthorById(c *fiber.Ctx) error {
 		"data":    author,
 	})
 }
+
 func UpdateAuthor(c *fiber.Ctx) error {
-	return nil
+	db := database.DB
+	id := c.Params("authorId")
+	var author models.Author
+	err := db.First(&author, id).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "id tidak ditemukan",
+			"error":   err.Error(),
+		})
+	}
+	var updatedAuthor models.Author
+	err = c.BodyParser(&updatedAuthor)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "bad gateway",
+			"error":   err.Error(),
+		})
+	}
+	if updatedAuthor.Nama == "" || updatedAuthor.Usia == 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Nama dan Usia tidak boleh kosong",
+		})
+	}
+	author.Nama = updatedAuthor.Nama
+	author.Usia = updatedAuthor.Usia
+	err = db.Save(&author).Error
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "gagal memperbarui data author, lengkapi data",
+			"error":   err.Error(),
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"succes":  "true",
+		"message": "berhasil memperbarui data author",
+		"data": author,
+	})
 }
+
 func DeleteAuthor(c *fiber.Ctx) error {
 	db := database.DB
 	id := c.Params("authorId")
 	var deletedAuthor models.Author
 	err := db.First(&deletedAuthor, id).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return c.Status(404).JSON(fiber.Map{
-				"message": "id tidak ditemukan",
-			})
-		}
-		return c.Status(500).JSON(fiber.Map{
-			"message": "bad gateway",
+		return c.Status(404).JSON(fiber.Map{
+			"message": "id tidak ditemukan",
 			"error":   err.Error(),
 		})
 	}
+
 	err = db.Delete(&deletedAuthor, id).Error
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
